c08b7d087e5a11299f18e6250594c16518a1e51c
Nikita_Osmkinkin <nik-os2001@mail.ru>
2022-12-24 15:38:05 +0400

    success project

--- src/main/java/ru/osminkin/springvideohosting/model/User.java
+++ src/main/java/ru/osminkin/springvideohosting/model/User.java
@@ -15,6 +15,7 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "email")
     private String email;
     @Column(name = "password")
